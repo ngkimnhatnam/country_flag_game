8410ce19352802145ba966a86755f5cb6142c1af
Meo <ngkimnhatnam@gmail.com>
2020-06-30 12:29:25 +0000

    fix indentation

--- src/Replay.js
+++ src/Replay.js
@@ -6,10 +6,10 @@ class Replay extends Component {
     const {onHandleFetchAll, correctOne, guessStatus} = this.props;
     return(
       <div className='replay'>
-				{guessStatus===1 ? 
-					<p className='congrats'>You got it</p>:
-					<p className='wrong'>Oh oh! Correct answer:</p>
-				}
+        {guessStatus===1 ? 
+          <p className='congrats'>You got it</p>:
+          <p className='wrong'>Oh oh! Correct answer:</p>
+        }
         <p className='correct'>{correctOne.name}</p>
         <button onClick={onHandleFetchAll}>Again?</button>
       </div>
